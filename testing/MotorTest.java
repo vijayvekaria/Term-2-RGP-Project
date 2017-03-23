ea5fb00f10449e8aba4d46c641818372e29ecfed
ismailshak <ismailshak94@yahoo.com>
2017-03-23 09:48:38 +0000

    Update MotorTest.java

--- testing/MotorTest.java
+++ testing/MotorTest.java
@@ -2,9 +2,6 @@ package testing;
 
 public class MotorTest 
 {
-	
-	
-	//off by 0.5mm
 	static RegulatedMotor leftMotor = new EV3LargeRegulatedMotor(MotorPort.A);
 	static RegulatedMotor rightMotor = new EV3LargeRegulatedMotor(MotorPort.D);
 	
@@ -21,44 +18,39 @@ public class MotorTest
 	 
 	public static void main(String[] args) 
 	{
-		
-		//forwardTest();
-		
 		MotorTest test = new MotorTest();
-		test.rotationTest();
-	
-		
+		//test.forwardTest();
 	}
 
 	private void rotationTest() 
 	{
 		for(int i = 0; i < 4; i++)
-		{
-//			leftMotor.setSpeed(102); //(614/2)
-//			rightMotor.setSpeed(102);
-//			
-//			leftMotor.synchronizeWith(syncList);
-//			leftMotor.startSynchronization();
-//			
-//			leftMotor.forward();
-//			rightMotor.forward();
-//			
-//			leftMotor.endSynchronization();
-//			
-//			try {
-//				Thread.sleep(1000);
-//			} catch (InterruptedException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
-//			
-//			leftMotor.synchronizeWith(syncList);
-//			leftMotor.startSynchronization();
-//			
-//			leftMotor.stop();
-//			rightMotor.stop();
-//			
-//			leftMotor.endSynchronization();
+// 		{
+// //			leftMotor.setSpeed(102); //(614/2) which is 30cm per 2 seconds
+// //			rightMotor.setSpeed(102);
+// //			
+// //			leftMotor.synchronizeWith(syncList);
+// //			leftMotor.startSynchronization();
+// //			
+// //			leftMotor.forward();
+// //			rightMotor.forward();
+// //			
+// //			leftMotor.endSynchronization();
+// //			
+// //			try {
+// //				Thread.sleep(2000);
+// //			} catch (InterruptedException e) {
+// //				// TODO Auto-generated catch block
+// //				e.printStackTrace();
+// //			}
+// //			
+// //			leftMotor.synchronizeWith(syncList);
+// //			leftMotor.startSynchronization();
+// //			
+// //			leftMotor.stop();
+// //			rightMotor.stop();
+// //			
+// //			leftMotor.endSynchronization();
 			
 			rotate(1);
 			
@@ -113,16 +105,6 @@ public class MotorTest
 				gyroProvider.fetchSample(gyroSample, 0);
 			}
 		}
-		
-		
-		
-		
-//		try {
-//			Thread.sleep(2000);
-//		} catch (InterruptedException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
 
 		leftMotor.synchronizeWith(syncList);
 		leftMotor.startSynchronization();
@@ -134,8 +116,10 @@ public class MotorTest
 	}
 
 	public void forwardTest() {
-		leftMotor.setSpeed(41);
+		
+		leftMotor.setSpeed(41); //2cm per second
 		rightMotor.setSpeed(41);
+		
 		for(int i = 0; i < 10; i++)
 		{
 			leftMotor.synchronizeWith(syncList);
@@ -162,174 +146,7 @@ public class MotorTest
 			leftMotor.endSynchronization();
 			
 			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	}
-
-}
- 0.5mm
-	static RegulatedMotor leftMotor = new EV3LargeRegulatedMotor(MotorPort.A);
-	static RegulatedMotor rightMotor = new EV3LargeRegulatedMotor(MotorPort.D);
-	
-	static DifferentialPilot dPilot = new DifferentialPilot(56, 120, leftMotor, rightMotor);
-	
-	static RegulatedMotor[] syncList = {rightMotor};
-	
-	Port gyroPort = LocalEV3.get().getPort("S1");
-	EV3GyroSensor gyro = new EV3GyroSensor(gyroPort);
-	SampleProvider gyroProvider = gyro.getAngleMode();	
-	float[] gyroSample = new float[gyroProvider.sampleSize()];
-	
-	TextLCD screen = LocalEV3.get().getTextLCD();
-	 
-	public static void main(String[] args) 
-	{
-		
-		//forwardTest();
-		
-		MotorTest test = new MotorTest();
-		test.rotationTest();
-	
-		
-	}
-
-	private void rotationTest() 
-	{
-		for(int i = 0; i < 4; i++)
-		{
-//			leftMotor.setSpeed(102); //(614/2)
-//			rightMotor.setSpeed(102);
-//			
-//			leftMotor.synchronizeWith(syncList);
-//			leftMotor.startSynchronization();
-//			
-//			leftMotor.forward();
-//			rightMotor.forward();
-//			
-//			leftMotor.endSynchronization();
-//			
-//			try {
-//				Thread.sleep(1000);
-//			} catch (InterruptedException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
-//			
-//			leftMotor.synchronizeWith(syncList);
-//			leftMotor.startSynchronization();
-//			
-//			leftMotor.stop();
-//			rightMotor.stop();
-//			
-//			leftMotor.endSynchronization();
-			
-			rotate(1);
-			
-			try {
-				Thread.sleep(500);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-	
-		}
-	}
-
-	public void rotate(int direction) {
-		
-		gyro.reset();
-		
-		leftMotor.setSpeed(40);
-		rightMotor.setSpeed(40);
-		
-		leftMotor.synchronizeWith(syncList);
-		leftMotor.startSynchronization();
-		
-		gyroProvider.fetchSample(gyroSample, 0);
-		
-		if (direction == 1)//CLOWCKWISE
-		{ 
-			leftMotor.forward();
-			rightMotor.backward();
-		} 
-		else  //ANTICLOCKWISE
-		{
-			leftMotor.backward();
-			rightMotor.forward();
-		}
-		
-		leftMotor.endSynchronization();
-		
-		if (direction == 1)
-		{
-			while(gyroSample[0] <= 87)
-			{
-				screen.drawString(gyroSample[0] + "", 0, 1);
-				gyroProvider.fetchSample(gyroSample, 0);
-			}
-		} 
-		else 
-		{
-			while(gyroSample[0] >= -87)
-			{
-				screen.drawString(gyroSample[0] + "", 0, 1);
-				gyroProvider.fetchSample(gyroSample, 0);
-			}
-		}
-		
-		
-		
-		
-//		try {
-//			Thread.sleep(2000);
-//		} catch (InterruptedException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-
-		leftMotor.synchronizeWith(syncList);
-		leftMotor.startSynchronization();
-		
-		leftMotor.stop();
-		rightMotor.stop();
-		
-		leftMotor.endSynchronization();
-	}
-
-	public void forwardTest() {
-		leftMotor.setSpeed(41);
-		rightMotor.setSpeed(41);
-		for(int i = 0; i < 10; i++)
-		{
-			leftMotor.synchronizeWith(syncList);
-			leftMotor.startSynchronization();
-			
-			leftMotor.forward();
-			rightMotor.forward();
-			
-			leftMotor.endSynchronization();
-			
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			
-			leftMotor.startSynchronization();
-			
-			leftMotor.stop();
-			rightMotor.stop();
-			
-			leftMotor.endSynchronization();
-			
-			try {
-				Thread.sleep(1000);
+				Thread.sleep(8000);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
